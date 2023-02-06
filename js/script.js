5f9a1d76fdad2d5acf974b6b798983b97fe1bd69
Olesya Figurovskaya <lisonkadrug@gmail.com>
2023-02-06 10:36:53 +0300

    footer homePage

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+$('.homePage__carousel__slider').slick({
+    slidesToShow: 1,
+    arrows: true,
+    dots: true,
+    slidesToScroll: 1,
+    easing: 'ease',
+    infinite: true,
+    autoplay: true,
+    autoPlaySpeed: 1000,
+    touchTreshHold: 15,
+    waitForAnimate: false,
+    centerMode: false,
+    cssEase: 'ease-in',
+});
+});
+
+$(document).ready(function() {
+    $('.homePage__exclusive__slider').slick({
+        slidesToShow: 3.95,
+        arrows: true,
+        dots: false,
+        slidesToScroll: 1,
+        easing: 'ease',
+        infinite: true,
+        autoplay: false,
+        autoPlaySpeed: 1000,
+        touchTreshHold: 15,
+        waitForAnimate: false,
+        centerMode: false,
+        cssEase: 'ease-in',
+    });
+    });
+
+
